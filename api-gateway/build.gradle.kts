a6b4afbecde99347fa929864a7c9c0dbf0e16027
twink618 <twinky618@gmail.com>
2024-05-25 19:40:34 +0300

    add simple api-gateway

--- api-gateway/build.gradle.kts
+++ api-gateway/build.gradle.kts
@@ -15,8 +15,9 @@ repositories {
 val springCloudVersion = "2023.0.1"
 
 dependencies {
-    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-server:4.1.1")
-    testImplementation("org.springframework.boot:spring-boot-starter-test:3.3.0")
+    implementation("org.springframework.cloud:spring-cloud-starter-gateway:4.1.3")
+    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client:4.1.1")
+    implementation("org.springframework.boot:spring-boot-starter-webflux:3.3.0")
 }
 
 dependencyManagement {
