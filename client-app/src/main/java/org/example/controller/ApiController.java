892a39c00c02f0a13b9b9a779ab2d25517f42ed3
twink618 <twinky618@gmail.com>
2024-05-25 20:16:26 +0300

    add routing

--- client-app/src/main/java/org/example/controller/ApiController.java
+++ client-app/src/main/java/org/example/controller/ApiController.java
@@ -1,13 +1,19 @@
 package org.example.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class ApiController {
+
+    @Value("${eureka.instance.instance-id}")
+    private String port;
 
     @GetMapping("/healthCheck")
     public String healthCheck() {
-        return "OK";
+        return port;
     }
 }
